b11f90ddb5e6b8a619a9e4ce9cc680077c309b0b
HelloWorld <sc21m2w@leeds.ac.uk>
2024-11-23 14:36:01 +0800

    Improve security by preventing SQL injection and XSS attacks
    - Replaced raw SQL queries with PreparedStatements to prevent SQL injection.
    - Added MD5 hashing for user passwords before authentication to enhance security.
    - Updated HTML templates to include ?html escaping to prevent XSS attacks.

--- src/main/java/comp3911/cwk2/AppServlet.java
+++ src/main/java/comp3911/cwk2/AppServlet.java
@@ -2,11 +2,12 @@ package comp3911.cwk2;
 
 import java.io.File;
 import java.io.IOException;
+import java.security.MessageDigest;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -26,8 +27,8 @@ import freemarker.template.TemplateExceptionHandler;
 public class AppServlet extends HttpServlet {
 
   private static final String CONNECTION_URL = "jdbc:sqlite:db.sqlite3";
-  private static final String AUTH_QUERY = "select * from user where username='%s' and password='%s'";
-  private static final String SEARCH_QUERY = "select * from patient where surname='%s' collate nocase";
+  private static final String AUTH_QUERY = "select * from user where username=? and password=?";
+  private static final String SEARCH_QUERY = "select * from patient where surname=? collate nocase";
 
   private final Configuration fm = new Configuration(Configuration.VERSION_2_3_28);
   private Connection database;
@@ -81,7 +82,20 @@ public class AppServlet extends HttpServlet {
     String username = request.getParameter("username");
     String password = request.getParameter("password");
     String surname = request.getParameter("surname");
-
+    try{
+      MessageDigest md = MessageDigest.getInstance("MD5");
+      md.update(password.getBytes());
+      byte[] digest = md.digest();
+      // 将字节转换为十六进制字符串
+      StringBuilder sb = new StringBuilder();
+      for (byte b : digest) {
+        sb.append(String.format("%02x", b));
+      }
+      password = sb.toString();
+      System.out.println(password);
+    }catch (Exception error){
+      response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    }
     try {
       if (authenticated(username, password)) {
         // Get search results and merge with template
@@ -103,28 +117,28 @@ public class AppServlet extends HttpServlet {
   }
 
   private boolean authenticated(String username, String password) throws SQLException {
-    String query = String.format(AUTH_QUERY, username, password);
-    try (Statement stmt = database.createStatement()) {
-      ResultSet results = stmt.executeQuery(query);
-      return results.next();
-    }
+    PreparedStatement query = database.prepareStatement(AUTH_QUERY);
+    query.setString(1, username);
+    query.setString(2, password);
+    ResultSet results = query.executeQuery();
+    return results.next();
   }
 
   private List<Record> searchResults(String surname) throws SQLException {
     List<Record> records = new ArrayList<>();
-    String query = String.format(SEARCH_QUERY, surname);
-    try (Statement stmt = database.createStatement()) {
-      ResultSet results = stmt.executeQuery(query);
-      while (results.next()) {
-        Record rec = new Record();
-        rec.setSurname(results.getString(2));
-        rec.setForename(results.getString(3));
-        rec.setAddress(results.getString(4));
-        rec.setDateOfBirth(results.getString(5));
-        rec.setDoctorId(results.getString(6));
-        rec.setDiagnosis(results.getString(7));
-        records.add(rec);
-      }
+    // String query = String.format(SEARCH_QUERY, surname);
+    PreparedStatement query = database.prepareStatement(SEARCH_QUERY);
+    query.setString(1, surname);
+    ResultSet results = query.executeQuery();
+    while (results.next()) {
+      Record rec = new Record();
+      rec.setSurname(results.getString(2));
+      rec.setForename(results.getString(3));
+      rec.setAddress(results.getString(4));
+      rec.setDateOfBirth(results.getString(5));
+      rec.setDoctorId(results.getString(6));
+      rec.setDiagnosis(results.getString(7));
+      records.add(rec);
     }
     return records;
   }
