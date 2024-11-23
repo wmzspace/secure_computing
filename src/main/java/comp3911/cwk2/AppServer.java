2a889568cd8a3087db9cdb74792013698c455469
HelloWorld <sc21m2w@leeds.ac.uk>
2024-11-23 18:42:39 +0800

    Add thread pool and ServerConnector configuration to improve server performance.

--- src/main/java/comp3911/cwk2/AppServer.java
+++ src/main/java/comp3911/cwk2/AppServer.java
@@ -1,7 +1,9 @@
 package comp3911.cwk2;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
 import org.eclipse.jetty.util.log.Log;
 import org.eclipse.jetty.util.log.StdErrLog;
 
@@ -9,12 +11,27 @@ public class AppServer {
   public static void main(String[] args) throws Exception {
     Log.setLog(new StdErrLog());
 
+    // 创建线程池，设置线程池的最小和最大线程数
+    QueuedThreadPool threadPool = new QueuedThreadPool();
+    threadPool.setMaxThreads(100); // 最大线程数
+    threadPool.setMinThreads(10); // 最小线程数
+    threadPool.setIdleTimeout(30000); // 空闲线程超时时间（毫秒）
+
+    // 创建服务器并使用线程池
+    Server server = new Server(threadPool);
+
+    // 创建 ServerConnector 并设置端口
+    ServerConnector connector = new ServerConnector(server);
+    connector.setPort(8080); // 设置端口为 8080
+    server.addConnector(connector);
+
+    // 配置 ServletHandler
     ServletHandler handler = new ServletHandler();
     handler.addServletWithMapping(AppServlet.class, "/*");
 
-    Server server = new Server(8080);
     server.setHandler(handler);
 
+    // 启动服务器
     server.start();
     server.join();
   }
